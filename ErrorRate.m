c13b6f5da439b359199a049b0d0c9b9f89886e4d
LaZol <laura.zollo00@gmail.com>
2018-12-24 10:34:01 +0000

    Update ErrorRate.m

--- ErrorRate.m
+++ ErrorRate.m
@@ -1,8 +1,11 @@
 %%calcolare Error rate
 
 function [outputArg1,outputArg2] = ErrorRate(inputArg1,inputArg2)
-%ERRORRATE Summary of this function goes here
-%   Detailed explanation goes here
+%ERRORRATE 
+%INPUT: l'originale e la decifrata (stesse dimensioni)
+%OUTPUT: valore dell'energia media dell'errore.
+%l'errore medio è calcolato solo sugli utlimi 3 bit di ogni pixel, in decimale (o anche sul valore intero del pixel)
+%è la sommatoria della differenza al quadrato degli ultimi 3 bit di ogni pixel, diviso il numero di pixel
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
