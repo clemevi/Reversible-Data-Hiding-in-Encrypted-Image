5f1998cb805d5c22993960f10c18b3fea2b5f4ef
Clementevigliotti92 <clementevigliotti92@gmail.com>
2019-01-04 16:04:52 +0000

    Update ImageDecryption.m

--- ImageDecryption.m
+++ ImageDecryption.m
@@ -5,16 +5,20 @@ function M_dec = ImageDecryption(M, K)
 % M utilizzando lo streamcipher usato per la cifratura.
 
 %get Image size
-row, col = size(M);
-n = row*col;
+[row, col] = size(M);
+n = row*col*8;
 
 %get pseudorandom bit string by streamcipher (trivium)
 pseudorandom_bit = trivium(n,K);
+%separeted bit to group of 8 bit (int)
+pad = vec2mat(pseudorandom_bit, 8);
+%convert bit to int
+pad_int = (bin2dec(int2str(pad)))';
 
 %convert vector obateined by trivium to matrix
-mat_bit = vec2mat(pseudorandom_bit, col);
+mat_bit = vec2mat(pad_int, col);
 
 %do bit-wise xor beteween Image and trivium output
-M_dec = bitxor(M , mat_bit);
+M_dec = bitxor(M , uint8(mat_bit));
 
 end
