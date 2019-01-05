39836b3c68f6e9f2df696e1e6991813b9f991032
therounders91 <kesquadro@gmail.com>
2019-01-05 17:29:41 +0000

    Update FlipBlock.m

--- FlipBlock.m
+++ FlipBlock.m
@@ -3,28 +3,31 @@ function Bflip = FlipBlock(B,Kh,b)
 % Data una matrice di interi B, una chiave Kh e un bit b, inizializzare
 % lo streamcipher con la chiave Kh, flippare gli ultimi 3 bit 
 % dell'iesimo elemento di B se l'i-esimo elemento dato dallo streamcipher
-% è uguale a b. Restituire la matrice con i bit flippati.
+% Ã¨ uguale a b. Restituire la matrice con i bit flippati.
 
 
 %%inizializzo lo streamcipher con la 
 %%chiave Kh richiamando la funzione
 %%trivium
-streamCipher = trivium(n,Kh);
-[J,K] = size(streamCipher);
 
-%%Scorro le due matrici e se l'iesimo elemento di streamcipher 
-%%è uguale a b allora flippo gli ultimi 3 bit dell'iesimo elemento di B
+[M,N] = size(B);
+l = M*N;
+streamCipher = trivium(l,Kh);
+pad = vec2mat(streamCipher,N);
 
-for x=1:J
-    for z=1:K
-        if streamCipher(x,z) == b
+%%Scorro streamcipher e se l'iesimo elemento
+%%Ã¨ uguale a b allora flippo gli ultimi 3 bit dell'iesimo elemento di B
+
+for x=1:M
+    for z=1:N
+        if pad(x,z) == b
            pixel = B(x,z);
            temp = bitget(pixel,1:3);
            temp2 = bitcmp(temp, 1:3);
            B(x,z) = temp2;
            Bflip(x,z) = B(x,z);
         else
-           Bflip = B(x,z);
+           Bflip(x,z) = B(x,z);
         end
     end
 
