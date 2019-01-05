62ab0afe580f1c477b086fe3f632b1b270ccfefa
LaZol <laura.zollo00@gmail.com>
2019-01-05 17:20:46 +0000

    Upload New File

---
+++ TEST/TESTcalculateFluctuation.m
@@ -0,0 +1,16 @@
+a =[3 3 3;3 3 3; 3 3 3]
+fluct_a = calculateFluctuation(a);
+
+b =[3 0 3;0 3 0; 3 0 3]
+fluct_b = calculateFluctuation(b);
+
+Img = imread('C:\Users\laura\Desktop\Magistrale\CD\lena512.bmp');
+if size(Img,3)==3
+    Img = rgb2gray(Img)
+end
+M = uint8(Img);
+block = getBlock(M, 32, 10);
+imshow(M)
+figure
+imshow(block)
+fluct_block = calculateFluctuation(block);
