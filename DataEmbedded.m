c6929c17635c3989762be3cd984d1b4129911bb6
therounders91 <kesquadro@gmail.com>
2019-01-07 15:34:39 +0000

    Update DataEmbedded.m

--- DataEmbedded.m
+++ DataEmbedded.m
@@ -1,11 +1,23 @@
-function [outputArg1,outputArg2] = DataEmbedded(inputArg1,inputArg2)
-% DATA EMBEDDED:
-% DATA UNA STRINGA DI BIT 'data', una matrice di interi M e una chiave Kh,
-% richiamare SplitImage e per ogni Blocck Richiamare FlipBlock
-% dandogli in input l'iesimo bit della stringa 'data', Restituire la
-% matrice M con in bit flippati
+function  flippedMatrix = Data_Embedded(data,M,Kh)
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%%Data una stringa di bit "data" , una matrice di interi M e una chiave Kh
+%%Richiamare SplitImage e per ogni blocco:
+%%Richiamare Flipblock dandogli in input l'iesimo bit della stringa "data"
+%%Restituire la matrice M con i bit flippati
+
+block = getBlock(M,s,k);
+[i,j] = size(block);
+l= size(data);
+
+for x=1:i
+    for y=1:j
+        for z=1:l
+        flip = FlipBlock(M,Kh,data(l));
+        flippedMatrix(x,y) = flip;
+        end
+    end
+end
+
+
 end
 
