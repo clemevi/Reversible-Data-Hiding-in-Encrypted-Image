ba45599fbfd45917a72c8a501153f27e4116e1ae
LaZol <laura.zollo00@gmail.com>
2019-01-08 15:26:05 +0000

    Replace getBlock.m

--- getBlock.m
+++ getBlock.m
@@ -1,32 +1,31 @@
-function [B] = getBlock(M, s, k)
-%getBlock return the k block of M the image, s is the dimension of blocks.
-%M dimensions must be divisible by k
+function [B] = getBlock(M, s, i, j)
+%getBlock return the k block of M matrix bidimensional image, s is the dimension of blocks.
+%M dimensions must be divisible by s
 
-%calculate rows and cols of image
 h = size(M, 1);
 w = size(M, 2);
 linear_size = h*w;
 if mod(linear_size, s)~=0
-   fprintf(">>>>>>>>>>Dimension of image M is not divisible by index k.<<<<<<<<<<\n");
+   fprintf(">>>>>>>>>>Dimension of image M is not divisible by s.<<<<<<<<<<\n");
    return;
 end
-block_num =(h*w/(s*s));
 
-%k can assume values from 0 to  block_num - 1
-if (k < 0 || k > block_num - 1)
-    fprintf(">>>>>>>>>>Invalid index k.<<<<<<<<<<\n");
+rows=h/s;
+cols=w/s;
+
+
+if (i < 0 || i > rows - 1)
+    fprintf(">>>>>>>>>>Invalid index i.<<<<<<<<<<\n");
+    return;
+end
+if (j < 0 || j> cols - 1)
+    fprintf(">>>>>>>>>>Invalid index j.<<<<<<<<<<\n");
     return;
 end
 
-%calculate number of clos and rows of blocks
-col = w/s;
-row = h/s;
-
-%translate k in 2D coordinates
-i = fix(k/col);
-j = fix(k/row);
-
 %get B block from 2D coordinates
 B = M(i*s+1 : i*s+s, j*s+1 : j*s+s);
+
+
 end
 
