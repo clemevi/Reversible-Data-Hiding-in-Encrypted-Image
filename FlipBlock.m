584c50ed4c2e3bcb5cf101f16dcf58a10a60b782
Clementevigliotti92 <clementevigliotti92@gmail.com>
2019-01-09 11:17:58 +0000

    Update FlipBlock.m

--- FlipBlock.m
+++ FlipBlock.m
@@ -3,13 +3,13 @@ function Bflip = FlipBlock(B,Kh,b)
 % Data una matrice di interi B, una chiave Kh e un bit b, inizializzare
 % lo streamcipher con la chiave Kh, flippare gli ultimi 3 bit 
 % dell'iesimo elemento di B se l'i-esimo elemento dato dallo streamcipher
-% Ã¨ uguale a b. Restituire la matrice con i bit flippati.
+% ÃƒÂ¨ uguale a b. Restituire la matrice con i bit flippati.
 
 
 %%inizializzo lo streamcipher con la 
 %%chiave Kh richiamando la funzione
 %%PRGA
-
+%tic
 [M,N] = size(B);
 l = ((M*N)/8)+1;
 r = mod(M*N,8);
@@ -17,23 +17,18 @@ pad_int = PRGA(Kh,l);
 pad = de2bi(pad_int, 8);
 pad = pad(1:end-(8-r));
 pad = reshape(pad,[M,N]);
-
+%time_pad = toc
 %%Scorro streamcipher e se l'iesimo elemento
-%%Ã¨ uguale a b allora flippo gli ultimi 3 bit dell'iesimo elemento di B
-
+%%ÃƒÂ¨ uguale a b allora flippo gli ultimi 3 bit dell'iesimo elemento di B
+%tic;
 for i=1:M
     for j=1:N
         if pad(i,j) == b
-           %%%%% flip bit %%%%
-           %get 3 LSB of B(i,j)
-           LSB = uint8(bitget(B(i,j),1:3));
-           %complement LSB
-           LSB_cmp = bitxor(LSB, uint8([1 1 1]));
-           B(i,j) = bi2de([LSB, bitget(B(i,j),4:8)]);
+            B(i,j) = bitxor(B(i,j),7);
         end
     end
 end
-
+%time_for = toc
 Bflip = uint8(B);
 
 end
