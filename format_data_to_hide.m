b5726ff674cf9edcbe3e9220d197a2da42c579ef
LaZol <laura.zollo00@gmail.com>
2019-01-09 12:08:08 +0000

    funzione per fomrattare la stringa da inserire

---
+++ format_data_to_hide.m
@@ -0,0 +1,7 @@
+function bit_data = format_data_to_hide(string)
+%Convert the char array to hide into bit string 
+dec_data = uint8(string);
+bit_data_to_reshape = de2bi(dec_data,8);
+bit_data = reshape(bit_data_to_reshape,[1, size(bit_data_to_reshape,1)*8]);
+end
+
