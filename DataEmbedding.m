0215c725ba72aa3240a9a4b817b1c68e369ef3ad
GiuseppeScotti92 <giuseppescotti92@gmail.com>
2019-01-14 17:43:35 +0000

    Update DataEmbedding.m

--- DataEmbedding.m
+++ DataEmbedding.m
@@ -1,8 +1,11 @@
 function  flippedMatrix = Data_Embedding(data,M,Kh)
-%%Data una stringa di bit "data" , una matrice di interi M e una chiave Kh
-%%Richiamare getblock e per ogni blocco:
-%%Richiamare Flipblock dandogli in input l'iesimo bit della stringa "data"
-%%Restituire la matrice M con i bit flippati
+%Input: Bit string, Two-dimensional matrix, Char string
+%Output: Two-dimensional matrix
+%Given the bits to hide, an image and a key
+%computes the size of an sxs block based on the image dimensions and the data to be inserted,
+%computes a pad matrix of the same size as a block containing pseudo-random bit (getMaskPad function),
+%for each image block z (getBlock) flips the block (FlipBlock) according to the pad and the z-th bit to insert
+%rebuilds the image with the flipped blocks (setBlock)
 [rows, cols] = size(M);
 bit_num = size(data, 2);
 
@@ -13,14 +16,18 @@ if mod(rows*cols,bit_num)~= 0
    return;
 end
 
+%computes the block size
 s = sqrt(rows*cols/bit_num);
+
 block_rows=rows/s;
 block_cols=cols/s;
 
 flippedMatrix = M;
 z = 1;
 
+%computes a pad matrix of the same size as a block containing pseudo-random bit
 pad = getMaskPad(s,Kh);
+
 
 for i=0:block_rows-1
     for j=0:block_cols-1 
