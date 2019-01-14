cfa7dc2c3c58485ac0b04b4b9ff2e4fc1b27f403
GiuseppeScotti92 <giuseppescotti92@gmail.com>
2019-01-14 18:06:29 +0000

    Update getMaskPad.m

--- getMaskPad.m
+++ getMaskPad.m
@@ -1,14 +1,18 @@
 function pad = getMaskPad(s,Kh)
-%%inizializzo lo streamcipher con la 
-%%chiave Kh richiamando la funzione
-%%PRGA per ottenere il pad
+%Input: Integer, Char string
+%Output: sxs matrix
+%calculates a pad matrix of the same size as a block containing pseudo-random bits computed with PRGA
+%as PRGA returns char, performs calculations to handle the discrepancy between s and the returned string
 
 %get lenght of pad
 l = uint64((floor((s*s)/8))+1);
+
 %get the remaining bits to add
 r = mod(s*s,8);
+
 %get pad
 pad_int = PRGA(Kh,l);
+
 %format the output
 pad = de2bi(pad_int, 8);
 pad = pad(1:end-(8-r));
