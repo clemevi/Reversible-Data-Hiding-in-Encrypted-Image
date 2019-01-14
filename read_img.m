209328ec062e58d0cd0c6a6aec73055ba75f60d7
GiuseppeScotti92 <giuseppescotti92@gmail.com>
2019-01-14 16:26:44 +0000

    Update read_img.m

--- read_img.m
+++ read_img.m
@@ -1,5 +1,7 @@
 function Img_8bit = read_img(img_path)
-%read image from path, convert in gray scale if necessary and set
+%Input: Image path
+%Output: Two-dimensional matrix with values between 0 and 255
+%Read image from path, convert in gray scale if necessary and set
 %profondity to 8 bit.
 
 Img = imread(img_path);
