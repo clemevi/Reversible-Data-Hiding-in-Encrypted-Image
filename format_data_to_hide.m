101fb236ecc459361782840ca3175b82ea8de39d
GiuseppeScotti92 <giuseppescotti92@gmail.com>
2019-01-14 16:21:00 +0000

    Update format_data_to_hide.m

--- format_data_to_hide.m
+++ format_data_to_hide.m
@@ -1,4 +1,6 @@
 function bit_data = format_data_to_hide(string)
+%Input:Characters string
+%Output: Bit string
 %Convert the char array to hide into bit string 
 dec_data = uint8(string);
 bit_data_to_reshape = de2bi(dec_data,8);
