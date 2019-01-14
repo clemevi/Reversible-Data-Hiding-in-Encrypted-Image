8c7b6b6dc86b7114ab584e4b0da7d9b97599cf09
GiuseppeScotti92 <giuseppescotti92@gmail.com>
2019-01-14 19:18:03 +0000

    Update PSNR.m

--- PSNR.m
+++ PSNR.m
@@ -1,6 +1,5 @@
 function peaksnr = PSNR(Img,ref)
-%PSNR Summary of this function goes here
-%   Detailed explanation goes here
+
 % PSNR = 10*LOG10((255^2)/(Err_ass/2))=37.9dB
 
 E = Error_Rate(Img,ref);
