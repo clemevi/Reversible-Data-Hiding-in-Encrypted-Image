93fbf9c154695dd7d03168bced6d3b4f654fe897
GiuseppeScotti92 <giuseppescotti92@gmail.com>
2019-01-14 16:57:02 +0000

    Update getRange.m

--- getRange.m
+++ getRange.m
@@ -1,4 +1,6 @@
 function range = getRange(M)
+%Input: Two-dimensional matrix
+%Output: Integers array
 % given an image M returns the different lengths (in bits) 
 % that can have the data to be hidden.
 [m,n] = size(M);
@@ -7,13 +9,14 @@ div_m = divisors(m);
 div_n = divisors(n);
 div = intersect(div_m,div_n);
 
-mask = div>=32;% & mod(div,8)==0;
+%To extract the data correctly, the minimum block size must be 32
+mask = div>=32;
 
 div = uint64(div(mask)) ;
 
-
 range = (m*n)./(div.^2);
 
+%To insert characters into the images, the lengths in bits must be divisible by 8
 mask = mod(range,8)==0;
 
 range = range(mask);
